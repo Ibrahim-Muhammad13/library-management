cd64ce1b26e333d5f979de2d59b709044a0cae55
ahmedmangood <ahmedmangood656@gmail.com>
2023-06-15 19:56:48 +0300

    create category page

---
+++ resources/views/category.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+@section('content')
+<div class="categories">
+<div class="container">
+    <h1 class="display-4 my-4">All Categories<h1>
+        <hr>
+    <div class="row">
+        <div class="col my-2">
+            <div class="card category-box" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">Category Name</h3>
+                    <a href="#" class="btn btn-primary btn-ourBtn">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col my-2">
+            <div class="card category-box" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">Category Name</h3>
+                    <a href="#" class="btn btn-primary btn-ourBtn">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col my-2">
+            <div class="card category-box" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">Category Name</h3>
+                    <a href="#" class="btn btn-primary btn-ourBtn">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col my-2">
+            <div class="card category-box" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">Category Name</h3>
+                    <a href="#" class="btn btn-primary btn-ourBtn">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col my-2">
+            <div class="card category-box" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">Category Name</h3>
+                    <a href="#" class="btn btn-primary btn-ourBtn">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col my-2">
+            <div class="card category-box" style="width: 18rem;">
+                <div class="card-body">
+                    <h3 class="card-title">Category Name</h3>
+                    <a href="#" class="btn btn-primary btn-ourBtn">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
