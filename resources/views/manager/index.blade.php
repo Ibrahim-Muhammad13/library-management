2a225387bac38d5c26cc036a50ce292345b15d26
ahmedmangood <ahmedmangood656@gmail.com>
2023-06-21 00:09:22 +0300

    updated manager style

--- resources/views/manager/index.blade.php
+++ resources/views/manager/index.blade.php
@@ -1,44 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-2" style="margin-top: 200px">@extends('layouts.side')</div>
-            <div class="col-10">
+<div class="container">
+    <div class="row">
+    <div class="col-2" style="margin-top: 200px">@extends('layouts.side')</div>
+        <div class="col-10">
+        <h3 class="display-3">Managers</h3>
+        <hr style="border: 1px solid">
         @if (session('success'))
-    <div class="alert alert-success" role="alert">
-        {{ session('success') }}
-    </div>
-@endif
-        <h1>Managers</h1>
-        <a href="{{ route('managers.create') }}" class="btn btn-primary mb-3">Create Manager</a>
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($managers as $manager)
+            <div class="alert alert-success" role="alert">
+                {{ session('success') }}
+            </div>
+        @endif
+        <div class="card bg-gray">
+            <a href="{{ route('managers.create') }}" class="btn btn-primary col-md-4 btn-category">Add Manager <i class="fas fa-plus"></i></a>
+            <div class="card-body">
+            <table class="table table-bordered text-center">
+                <thead>
                     <tr>
-                        <td>{{ $manager->id }}</td>
-                        <td>{{ $manager->name }}</td>
-                        <td>{{ $manager->email }}</td>
-                        <td>
-                            <a href="{{ route('managers.edit', $manager->id) }}" class="btn btn-sm btn-primary">Edit</a>
-                            <form action="{{ route('managers.destroy', $manager->id) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-danger">Delete</button>
-                            </form>
-                        </td>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                        <tbody>
+                            @foreach($managers as $manager)
+                                <tr>
+                                    <td>{{ $manager->id }}</td>
+                                    <td>{{ $manager->name }}</td>
+                                    <td>{{ $manager->email }}</td>
+                                    <td>
+                                        <a href="{{ route('managers.edit', $manager->id) }}" class="btn btn-success">Edit <i class="fas fa-pen-to-square"></i></a>
+                                        <form action="{{ route('managers.destroy', $manager->id) }}" method="POST" class="d-inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger">Delete <i class="fas fa-trash-can"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </div>
+                </table>
+        </div>
     </div>
+</div>
 @endsection
